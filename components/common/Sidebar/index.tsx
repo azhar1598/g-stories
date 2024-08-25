dd73ad38fab5a85a3e63144a0249365e21788ee5
azhar1598 <mohammedazhar.1598@gmail.com>
2024-08-25 19:16:59 +0530

    [ADD] stories

--- components/common/Sidebar/index.tsx
+++ components/common/Sidebar/index.tsx
@@ -1,23 +1,24 @@
 import { FC } from "react";
 import { IconApps, IconUser, IconWandOff } from "@tabler/icons-react";
+import { sidebarItems } from "@/sidebar";
+import Link from "next/link";
 
 const Sidebar: FC = () => {
   return (
     <div className="bg-gray-800 h-screen w-20 fixed top-10 flex flex-col justify-between">
-      <div className="flex flex-col items-center mt-4">
-        <div className="w-full flex flex-col items-center py-4 hover:bg-blue-600">
-          <div className="bg-blue-600 w-full flex flex-col items-center py-4">
-            <IconWandOff stroke={2} size={32} className="text-white" />
-            <span className="text-white text-sm">Generate</span>
-          </div>
-        </div>
-
-        <div className="w-full flex flex-col items-center py-4">
-          <div className="bg-gray-800 w-full flex flex-col  items-center py-4">
-            <IconUser size={32} className="text-white" />
-            <span className="text-white text-sm">Account</span>
-          </div>
-        </div>
+      <div className="flex flex-col items-center">
+        {sidebarItems.map((item, index) => (
+          <Link href={item.link} key={index} className="w-full">
+            <div
+              className={`w-full flex flex-col items-center py-4 ${
+                index === 0 ? "bg-[#394189]" : "hover:bg-[#394189]"
+              } cursor-pointer`}
+            >
+              <item.icon size={22} className="text-white" />
+              <span className="text-white text-xs pt-2">{item.name}</span>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
