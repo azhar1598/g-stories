bf2a511f2950ea3821bdffd7cd96ac040ca53619
azhar1598 <mohammedazhar.1598@gmail.com>
2024-10-16 17:56:28 +0530

    [ADD]

--- app/story-editor/MoveableComponent/index.tsx
+++ app/story-editor/MoveableComponent/index.tsx
@@ -8,7 +8,7 @@ const MoveableComponent = ({
   updateContent,
   selectedSlide,
   elementStyles,
-  isSelected,
+
   onSelect,
 }: any) => {
   const targetRef = useRef<HTMLDivElement>(null);
@@ -21,6 +21,9 @@ const MoveableComponent = ({
   const [isBold, setIsBold] = useState(item.styles?.fontWeight === "bold");
   const [isCenter, setIsCenter] = useState(item.styles?.textAlign === "center");
   const [isLocked, setIsLocked] = useState(false);
+
+  const [isSelected, setIsSelected] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const [currentStyles, setCurrentStyles] = useState(item.styles);
 
@@ -40,6 +43,8 @@ const MoveableComponent = ({
       !targetRef.current.contains(event.target as Node)
     ) {
       setIsEditable(false);
+      setIsSelected(false);
+      setIsHovered(false);
       adjustHeight();
     }
   };
@@ -153,6 +158,15 @@ const MoveableComponent = ({
         ref={targetRef}
         id={`moveable-${item.id}`}
         onDoubleClick={handleDoubleClick}
+        onClick={() => setIsSelected(true)}
+        onMouseEnter={() => {
+          setIsHovered(true);
+          if (!isSelected) setIsSelected(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false);
+          if (!isSelected) setIsSelected(false);
+        }}
         key={selectedSlide}
         style={{
           ...currentStyles,
@@ -160,9 +174,17 @@ const MoveableComponent = ({
           backgroundColor: item.styles?.backgroundColor || "",
           width: `${dimensions.width}px`,
           height: `${dimensions.height}px`,
-          border: isEditable ? "1px solid #ccc" : "1px solid transparent",
+          // border: isEditable ? "1px solid #ccc" : "1px solid transparent",
           position: "relative",
-          cursor: isLocked ? "not-allowed" : "grab",
+          cursor: isLocked ? "not-allowed" : "all-scroll",
+          border: isEditable
+            ? "1px solid #ccc"
+            : isSelected
+            ? "2px solid #3b82f6"
+            : isHovered
+            ? "1px dashed #9ca3af"
+            : "1px solid transparent",
+          zIndex: 10000,
         }}
       >
         <div className="absolute -top-10 z-[10000] left-0 right-0 bg-gray-800 rounded-t-md p-1 flex justify-between items-center">
@@ -222,34 +244,37 @@ const MoveableComponent = ({
         )}
       </div>
 
-      {!isEditable && selectedSlide && !isLocked && (
-        <Moveable
-          target={targetRef.current}
-          container={null}
-          origin={false}
-          edge={false}
-          draggable={true}
-          resizable={true}
-          scalable={false}
-          rotatable={false}
-          pinchable={false}
-          onDrag={({ target, transform }) => {
-            target!.style.transform = transform;
+      {!isEditable &&
+        selectedSlide &&
+        !isLocked &&
+        (isSelected || isHovered) && (
+          <Moveable
+            target={targetRef.current}
+            container={null}
+            origin={false}
+            edge={false}
+            draggable={true}
+            resizable={true}
+            scalable={false}
+            rotatable={false}
+            pinchable={false}
+            onDrag={({ target, transform }) => {
+              target!.style.transform = transform;
 
-            updateContent(item.id, {
-              styles: cssToJsx(targetRef.current?.style?.cssText),
-            });
-          }}
-          onResize={({ target, width, height, delta }) => {
-            if (delta[0]) target!.style.width = `${width}px`;
-            if (delta[1]) target!.style.height = `${height}px`;
-            setDimensions({ width, height });
-          }}
-          renderDirections={["e", "w"]}
-          keepRatio={false}
-          throttleResize={0}
-        />
-      )}
+              updateContent(item.id, {
+                styles: cssToJsx(targetRef.current?.style?.cssText),
+              });
+            }}
+            onResize={({ target, width, height, delta }) => {
+              if (delta[0]) target!.style.width = `${width}px`;
+              if (delta[1]) target!.style.height = `${height}px`;
+              setDimensions({ width, height });
+            }}
+            renderDirections={["e", "w"]}
+            keepRatio={false}
+            throttleResize={0}
+          />
+        )}
     </div>
   );
 };
